92fc40e319c69c282cd24e0334ee1e307be932a8
Fabio Veroli <fabio.veroli@studio.unibo.it>
2024-01-16 23:43:20 +0100

    Working function (missing filter, add in future)

--- connections/waterLevelSubSysMappingFunction.js
+++ connections/waterLevelSubSysMappingFunction.js
@@ -1,10 +1,16 @@
-function wlsMapToDittoProtocolMsg(headers, textPayload, bytePayload, contentType) {
+function mapToDittoProtocolMsg(headers, textPayload, bytePayload, contentType) {
     const jsonString = String.fromCharCode.apply(null, new Uint8Array(bytePayload)); 
     const jsonData = JSON.parse(jsonString); 
     const thingId = jsonData.thingId.split(':'); 
-
     if(headers['mqtt.topic'] === 'subsystems/org.eclipse.ditto:water-level-subsystem/status'){
-        return Ditto.buildDittoProtocolMsg( thingId[0], thingId[1], 'things','twin', 'commands','modify','/features/status/properties/status', headers, jsonData.status);
+        const status_wls = Ditto.buildDittoProtocolMsg( thingId[0], thingId[1], 'things','twin', 'commands','modify','/features/status/properties/status', headers, jsonData.status);
+        if(jsonData.status === 'ALARM'){
+            const sys_off = Ditto.buildDittoProtocolMsg( thingId[0], 'smart-light-subsystem', 'things','twin', 'commands','modify','/features/status/properties/status',headers, 'SYS_OFF');
+            return [ status_wls, sys_off];
+        } else {
+            const light_off = Ditto.buildDittoProtocolMsg( thingId[0], 'smart-light-subsystem', 'things','twin', 'commands','modify','/features/status/properties/status',headers, 'LIGHT_OFF');
+            return [status_wls, light_off];
+        }
     } else if (headers['mqtt.topic'] === 'subsystems/org.eclipse.ditto:water-level-subsystem/distance') {
         return Ditto.buildDittoProtocolMsg( thingId[0], thingId[1], 'things','twin', 'commands','modify','/features/water-level-sensor/properties/distance', headers, jsonData.distance);
     } else if (headers['mqtt.topic'] === 'subsystems/org.eclipse.ditto:water-level-subsystem/angle') {
@@ -12,10 +18,7 @@ function wlsMapToDittoProtocolMsg(headers, textPayload, bytePayload, contentType
     } else if (headers['mqtt.topic'] === 'subsystems/org.eclipse.ditto:water-level-subsystem/green') {
         return Ditto.buildDittoProtocolMsg( thingId[0], thingId[1], 'things','twin', 'commands','modify','/features/green-led/properties/on', headers, jsonData.on);
     } else if (headers['mqtt.topic'] === 'subsystems/org.eclipse.ditto:water-level-subsystem/red') {
-        const value = {
-            on : jsonData.on,
-            blinking : jsonData.blinking
-        };
+        const value = {on : jsonData.on,blinking : jsonData.blinking};
         return Ditto.buildDittoProtocolMsg( thingId[0], thingId[1], 'things','twin', 'commands','modify','/features/red-led/properties/', headers, value);
     } else {
         return null;
