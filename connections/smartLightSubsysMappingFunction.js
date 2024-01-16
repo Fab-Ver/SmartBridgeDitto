784b588d57d0ffa7e2f9873626099f4ecebd0699
Fabio Veroli <fabio.veroli@studio.unibo.it>
2024-01-16 15:39:48 +0100

    Create connection for smart light subsystem

---
+++ connections/smartLightSubsysMappingFunction.js
@@ -0,0 +1,15 @@
+function slsMapToDittoProtocolMsg(headers, textPayload, bytePayload, contentType) {
+    const jsonString = String.fromCharCode.apply(null, new Uint8Array(bytePayload)); 
+    const jsonData = JSON.parse(jsonString); 
+    const thingId = jsonData.thingId.split(':'); 
+
+    if(headers['mqtt.topic'] === 'subsystems/org.eclipse.ditto:smart-light-subsystem/status'){
+        return Ditto.buildDittoProtocolMsg( thingId[0], thingId[1], 'things','twin', 'commands','modify','/features/status/properties/status', headers, jsonData.status);
+    } else if (headers['mqtt.topic'] === 'subsystems/org.eclipse.ditto:smart-light-subsystem/dark') {
+        return Ditto.buildDittoProtocolMsg( thingId[0], thingId[1], 'things','twin', 'commands','modify','/features/light-sensor/properties/dark', headers, jsonData.dark);
+    } else if (headers['mqtt.topic'] === 'subsystems/org.eclipse.ditto:smart-light-subsystem/detected') {
+        return Ditto.buildDittoProtocolMsg( thingId[0], thingId[1], 'things','twin', 'commands','modify','/features/motion-sensor/properties/detected', headers, jsonData.detected);
+    } else {
+        return null;
+    }
+}
