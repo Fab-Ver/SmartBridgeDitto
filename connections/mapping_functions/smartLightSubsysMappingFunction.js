8dd5543c32dc69207ae1996c318e7d7d6edc69b9
Fabio Veroli <fabio.veroli@studio.unibo.it>
2024-01-17 18:47:07 +0100

    add light topic to function mapping

--- connections/mapping_functions/smartLightSubsysMappingFunction.js
+++ connections/mapping_functions/smartLightSubsysMappingFunction.js
@@ -3,8 +3,9 @@ function mapToDittoProtocolMsg(headers, textPayload, bytePayload, contentType) {
     const jsonData = JSON.parse(jsonString); 
     const thingId = jsonData.thingId.split(':'); 
 
-    if(headers['mqtt.topic'] === 'subsystems/org.eclipse.ditto:smart-light-subsystem/status'){
-        return Ditto.buildDittoProtocolMsg( thingId[0], thingId[1], 'things','twin', 'commands','modify','/features/status/properties/status', headers, jsonData.status);
+    if(headers['mqtt.topic'] === 'subsystems/org.eclipse.ditto:smart-light-subsystem/light'){
+        const value = {on : jsonData.on, waiting : jsonData.waiting};
+        return Ditto.buildDittoProtocolMsg( thingId[0], thingId[1], 'things','twin', 'commands','modify','/features/smart-light/properties/', headers, value);
     } else if (headers['mqtt.topic'] === 'subsystems/org.eclipse.ditto:smart-light-subsystem/dark') {
         return Ditto.buildDittoProtocolMsg( thingId[0], thingId[1], 'things','twin', 'commands','modify','/features/light-sensor/properties/dark', headers, jsonData.dark);
     } else if (headers['mqtt.topic'] === 'subsystems/org.eclipse.ditto:smart-light-subsystem/detected') {
